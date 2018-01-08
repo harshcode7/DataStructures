ccec9f0e2d9bf07b8204d6d45528dfdc21a88b6a
Harshad C <harshad7c@gmail.com>
2018-01-07 23:00:27 -0500

    update indentation

--- Sorting/QuickSort.cs
+++ Sorting/QuickSort.cs
@@ -2,11 +2,11 @@ using System;
 
 public class QuickSort
 {
-    public void Sort(int[] arr)
-    {
-        this.Sort(arr, 0, arr.Length - 1);
-    }
-   
+	public void Sort(int[] arr)
+	{
+		this.Sort(arr, 0, arr.Length - 1);
+	}
+
 	public void Sort(int[] arr, int left, int right)
 	{
 		if (left >= right)
@@ -15,7 +15,7 @@ public class QuickSort
 		}
 
 		int pivot = arr[right];
-        int pivotPos = left;
+		int pivotPos = left;
 		for (int i = left; i < right; i++)
 		{
 			if (arr[i] < pivot)
@@ -24,7 +24,7 @@ public class QuickSort
 			}
 		}
 
-        this.Swap(arr, right, pivotPos);
+		this.Swap(arr, right, pivotPos);
 		this.Sort(arr, left, pivotPos - 1);
 		this.Sort(arr, pivotPos + 1, right);
 	}
@@ -36,22 +36,22 @@ public class QuickSort
 		arr[y] = temp;
 	}
 
-    public void PrintArray(int[] arr)
-    {
-        for (int i = 0; i < arr.Length; i++)
-        {
-            Console.Write(arr[i] + " ");
-        }
+	public void PrintArray(int[] arr)
+	{
+		for (int i = 0; i < arr.Length; i++)
+		{
+			Console.Write(arr[i] + " ");
+		}
 
-        Console.WriteLine();
-    }
+		Console.WriteLine();
+	}
 
 	public static void Main()
 	{
 		int[] inp1 = { 5 };
 		int[] inp2 = { 5, 1 };
 		int[] inp3 = { 5, 3, 7 };
-        int[] inp4 = { 5, -2, 11, 3, 10, 12, 18, 21, 1, 6 };
+		int[] inp4 = { 5, -2, 11, 3, 10, 12, 18, 21, 1, 6 };
 
 		QuickSort qs = new QuickSort();
 		qs.Sort(inp1);
